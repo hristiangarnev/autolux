0c5525adffb23ed3528dd549e2c45ef5e76399d4
Hristian Garnev <hristiang@gmail.com>
2020-02-04 21:12:20 +0200

    Package.json changes

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react';
-import { createGlobalStyle } from 'styled-components'
+// import { createGlobalStyle } from 'styled-components'
 import Header from './Header';
 import Meta from './Meta';
 
-const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-  }
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-  body {
-    padding: 0;
-    margin: 0;
-    font-size: 16px;
-    line-height: 1.2;
-  }
-  a {
-    color: #000;
-    text-decoration: none;
-  }
-`;
+// const GlobalStyle = createGlobalStyle`
+//   html {
+//     box-sizing: border-box;
+//   }
+//   *, *:before, *:after {
+//     box-sizing: inherit;
+//   }
+//   body {
+//     padding: 0;
+//     margin: 0;
+//     font-size: 16px;
+//     line-height: 1.2;
+//   }
+//   a {
+//     color: #000;
+//     text-decoration: none;
+//   }
+// `;
 
 class Page extends Component {
   render() {
     return (
       <div id="wrapper">
         <Meta />
-        <GlobalStyle />
+        {/* <GlobalStyle /> */}
         <Header />
         {this.props.children}
       </div>
