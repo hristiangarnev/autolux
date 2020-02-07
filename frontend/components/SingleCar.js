6b1ddd55963daaa062d8e02cf9eebdd8f3c753af
Hristian Garnev <hristiang@gmail.com>
2020-02-07 23:37:37 +0200

    Single item page modifications

--- frontend/components/SingleCar.js
+++ frontend/components/SingleCar.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import Head from 'next/head';
+import styled from 'styled-components';
 import Loading from './Loading';
+
+const SingleCarElement = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+`;
 
 const SINGLE_CAR_QUERY = gql`
   query SINGLE_CAR_QUERY($id: ID!) {
@@ -21,13 +28,20 @@ class SingleCar extends Component {
       <Query query={SINGLE_CAR_QUERY} variables={{ id: this.props.id }}>
         {({data, error, loading}) => {
           if(loading) return <Loading />
+          if(!data.car) return <div>Error! No car with id {this.props.id} found!</div>
           if(error) return <p>Error... {error.message}</p>
+          
+          const car = data.car;
+          
           return (
-            <div className="car-view">
-              <h2>{data.car.title}</h2>
-              <img src={data.car.largeImage} />
-              <span>{data.car.price}</span>
-            </div>
+            <SingleCarElement>
+              <Head>
+                <title>{car.title} | AutoLux</title>
+              </Head>
+              <h2>{car.title}</h2>
+              <img src={car.largeImage} />
+              <span>{car.price}</span>
+            </SingleCarElement>
           )
         }}
       </Query>
