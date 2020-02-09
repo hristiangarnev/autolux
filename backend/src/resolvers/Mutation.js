2326bf3d3a2d58d8aeab0d2a7332b893733ca692
Hristian Garnev <hristiang@gmail.com>
2020-02-09 22:14:14 +0200

    Added logic for creating users

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
   async createCar(parent, args, ctx, info) {
     const car = await ctx.db.mutation.createCar({
@@ -24,6 +27,27 @@ const Mutation = {
     const car = await ctx.db.query.car({ where }, `{ id, title }`);
 
     return ctx.db.mutation.deleteCar({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      }
+    }, info);
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    return user;
   }
 };
 
