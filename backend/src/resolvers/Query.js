dd172d0a7f9458f44546bf205ec88bd3079abadb
Hristian Garnev <hristiang@gmail.com>
2020-02-09 20:50:28 +0200

    Pagination logic added

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   cars: forwardTo('db'),
-  car: forwardTo('db')
+  car: forwardTo('db'),
+  carsConnection: forwardTo('db')
 };
 
 module.exports = Query;
